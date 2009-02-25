744bd3b01b0ca5b329491768027885a9efdd58b8
George Plsek <george@gtekg.com>
2009-02-24 18:52:20 -0800

    updated wp-config

--- wp-config.php
+++ wp-config.php
@@ -27,7 +27,7 @@ define('DB_USER', 'specialstage');
 define('DB_PASSWORD', '12Sunset');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
